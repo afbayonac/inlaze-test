e1652cd5bce5eb7362d88243f4ab3d82786c9494
aratoca <afbayonac@gmail.com>
2022-12-26 20:59:38 -0500

    Add logo and hero image

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../assets/image/hero.png'
+import Logo from '../assets/logo.svg'
 import { Inter } from '@next/font/google'
 
 
@@ -12,7 +14,12 @@ export default function Home() {
         <title>Wires</title>
       </Head>
       <main>
-        Wires
+        <Image alt='cubes background' src={Hero}  width="375" height="375"></Image>
+        <Image alt='wires logo' src={Logo}  width="375" height="375"></Image>
+        <button>---</button>
+        <input></input>
+        <input></input>
+        <button>---</button>
       </main>
     </>
   )
